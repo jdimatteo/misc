ee080dda82a7d3f6abfbd2c60e7dc57a200d9bd7
John DiMatteo <jdimatteo@gmail.com>
2013-08-17 19:27:57 -0400

    Updated job to use new table supplied by Charles

--- Bam_Plot/bamPlotArgumentsFromTable.py
+++ Bam_Plot/bamPlotArgumentsFromTable.py
@@ -3,61 +3,44 @@
 import csv
 import sys
 
-def isRgbValue(value):
-    try:
-        return 0 <= int(value) <= 255
-    except ValueError:
-        return False
-
-if len(sys.argv) != 4:
-    print "Usage: %s <table_data_file> <name_selection_from_data_file> <color_override>" % sys.argv[0]
+if len(sys.argv) != 3:
+    print "Usage: %s <table_data_file> <name_selection_from_data_file>" % sys.argv[0]
     print ""
-    print "Returns the corresponding bam file, genome, and color in the following format: BAM_FILE -g GENOME -c COLOR" 
+    print "Returns the corresponding bam file and genome in the following format: BAM_FILE -g GENOME" 
     print ""
-    print "If the color override is not an empty string, then it is used."
+    print "Example:"
     print ""
-    print "Examples:"
-    print ""
-    print '\t$ %s /home/data/MM1S_Table.txt MM1S_BRD4_500nM_JQ1 ""' % sys.argv[0]
-    print "\t05242012_D0WUYACXX_3.ACAGTG.hg18.bwt.sorted.bam -g hg18 -c 47,26,251" 
-    print "\t$"
-    print ""
-    print '\t$ %s /home/data/MM1S_Table.txt MM1S_CDK9_500nM_JQ1 "255,132,0"' % sys.argv[0]
-    print "\t08282012_C1260ACXX_6.CGTACG.hg18.bwt.sorted.bam -g hg18 -c 255,132,0" 
+    print '\t$ %s /mnt/d0-0/share/bradnerlab/projects/masterBamTable.txt MM1S_BRD4_500nM_JQ1' % sys.argv[0]
+    print "\t/ifs/labs/bradner/bam/hg18/mm1s/05242012_D0WUYACXX_3.ACAGTG.hg18.bwt.sorted.bam -g HG18" 
     print "\t$"
     sys.exit(-1)
 
-(table_data_file_name, name, color_override) = (sys.argv[1], sys.argv[2], sys.argv[3])
+(table_data_file_name, name) = (sys.argv[1], sys.argv[2])
 
-# files are assumed to be in a format such as the following:
+# files are assumed to be in a 5 column tab delimited format such as the following:
+# GENOME  SOURCE  CELL_TYPE       NAME    BAMFILE
+# HG18    Diffuse large B-cell lymphoma   LY1     LY1_BCL6_DMSO   /ifs/labs/bradner/bam/hg18/ly1/06152012_C0VD4ACXX_7.GTCCGC.hg18.bwt.sorted.bam
+genome_col  = 0
+source_col  = 1 # ignored
+celltyp_col = 2 # ignored
+name_col    = 3
+file_col    = 4
+
+# old format:
 # NAME    UNIQUE_ID       BAM_FILE        GENOME  COLOR
 # MM1S_BRD4_500nM_JQ1     05242012_D0WUYACXX_3.ACAGTG     05242012_D0WUYACXX_3.ACAGTG.hg18.bwt.sorted.bam hg18    47,26,251
-name_col   = 0
-id_col     = 1
-file_col   = 2
-genome_col = 3
-color_col  = 4
+# name_col   = 0
+# id_col     = 1
+# file_col   = 2
+# genome_col = 3
+# color_col  = 4
 
 with open(table_data_file_name, 'r') as table_data_file:
     reader = csv.reader(table_data_file, delimiter='\t')
    
     for record in reader:
         if record[name_col] == name:
-            if color_override != '':
-                try:
-                    color = color_override.replace(" ", "") # remove any spaces a user may have erroneously added
-                    rgb = color.split(",")
-                    
-                    # verify that we have three integers between values of 0 and 256
-                    if len(rgb) != 3 or not isRgbValue(rgb[0]) or not isRgbValue(rgb[1]) or not isRgbValue(rgb[2]):
-                        raise Exception("invalid color: %s" % rgb)
-                except:
-                    print 'Color "%s" is invalid, defaulting to color in data table' % color_override
-                    color = record[color_col]
-            else:
-                color = record[color_col]
-
-            print "%s -g %s -c %s" % (record[file_col], record[genome_col], color) 
+            print "%s -g %s" % (record[file_col], record[genome_col]) 
             sys.exit(0)
 
 print "Record not found for name '%s'" % name
